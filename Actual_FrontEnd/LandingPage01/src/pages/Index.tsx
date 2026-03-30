b6156268871a4308f85ee063ebc59eaa5b1725ad
Pr1smVoid6 <pr1smvoid6@gmail.com>
2026-03-30 21:40:25 +0530

    Sorted Landing page

---
+++ Actual_FrontEnd/LandingPage01/src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import ScrollVideo from "@/components/ScrollVideo";
+
+const Index = () => {
+  return (
+    <div className="bg-background text-foreground">
+      <ScrollVideo />
+    </div>
+  );
+};
+
+export default Index;
